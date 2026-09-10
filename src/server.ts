53ac3e5e766c34f12014e8dbc46cf2cdb973e3b4
Walquiria Oliveira <waloa30@gmail.com>
2026-09-10 17:48:43 -0300

    feat: conecta servidor Express ao Banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,22 @@
-console.log('testando');
+import 'reflect-metadata';
+import express from 'express';
+
+//import { apiRouter } from './routes/api';
+import { initDatabaseConnection } from './database/data-source';
+
+async function main() {
+  const app = express();
+
+  await initDatabaseConnection();
+
+  const PORT = Number(process.env.PORT ?? 3000);
+
+  //app.use(express.json({ limit: '50mb' }));
+  //app.use('/api', apiRouter);
+
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT.toString()}`);
+  });
+}
+
+main().catch(console.error);
